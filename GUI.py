cde8b7a6bf4926e674782825cbb6e35d2ee53601
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-03-12 16:27:52 +0000

    Update GUI.py
    Added login menu which appears before you can start using the cash till operator. After successful login you can use the operator. User login "user" for user and "password" for password.

--- GUI.py
+++ GUI.py
@@ -1,3 +1,11 @@
+
+from tkinter import *
+
+from tkinter import messagebox
+# noinspection PyUnresolvedReferences
+from PIL import ImageTk, Image
+import sys #Imports sys, used to end the program later
+
 #Product Dictionary and variables
 
 Product_dictionary =	{
@@ -18,56 +26,11 @@ banana = float(1.2)
 ice = float(2)
 cereal = float(3.2)
 
-#Tkinter Main
-
-from tkinter import *
-
-from tkinter import messagebox
-# noinspection PyUnresolvedReferences
-from PIL import ImageTk, Image
-
-root = Tk()
-root.title("CASH till operator")
-root.geometry('800x600')
-leftFrame = Frame(root)
-leftFrame.pack(side=LEFT, expand=True, fill='both')
-rightFrame = Frame(root)
-rightFrame.pack(side=RIGHT, expand=True, fill='both')
-img = ImageTk.PhotoImage(Image.open(r"C:\Users\Vladimir\Desktop\test.jpg"))
-
-
-
-#GUI Main menu
-
-lbl = Label(leftFrame, text="Choose item", font=("Arial Bold", 40), bg="blue")
-lbl.pack(expand=True,fill='both')
-
-
-txt = Entry(leftFrame, width=10, bg="yellow", font=("Arial Bold", 40), fg="black")
-txt.pack(expand=True,fill='both')
-
-
 #Functions (button commands)
 
 def off_sale():
     messagebox.showwarning('auto_finder', 'not found try again')
     return
-
-def register():
-    res =  txt.get()
-    res2 = spin.get()
-    if res == banana_PLU:
-        sales_list.append(round(banana * float(res2), 2))
-        lbl.configure(text= sales_list)
-
-    elif res == ice_PLU:
-        sales_list.append(ice * float(res2))
-        lbl.configure(text= sales_list)
-    elif res == cereal_PLU:
-        sales_list.append(cereal * float(res2))
-        lbl.configure(text= sales_list)
-    else:
-        off_sale()
 
 def subtotal():
     total_PLU = sum(sales_list)
@@ -138,11 +101,43 @@ def find():
 def clear():
     txt.delete(first=0,last=100)
 
+def register():
+    res =  txt.get()
+    res2 = spin.get()
+    if res == banana_PLU:
+        sales_list.append(round(banana * float(res2), 2))
+        lbl.configure(text= sales_list)
+
+    elif res == ice_PLU:
+        sales_list.append(ice * float(res2))
+        lbl.configure(text= sales_list)
+    elif res == cereal_PLU:
+        sales_list.append(cereal * float(res2))
+        lbl.configure(text= sales_list)
+    else:
+        off_sale()
 
 
+#Tkinter login and main menu
 
+root=Tk() #Declares root as the tkinter main window
+top = Toplevel() #Creates the toplevel window
 
-
+entry1 = Entry(top) #Username entry
+entry2 = Entry(top) #Password entry
+button1 = Button(top, text="Login", command=lambda:command1()) #Login button
+button2 = Button(top, text="Cancel", command=lambda:command2()) #Cancel button
+root.title("CASH till operator")
+root.geometry('800x600')
+leftFrame = Frame(root)
+leftFrame.pack(side=LEFT, expand=True, fill='both')
+rightFrame = Frame(root)
+rightFrame.pack(side=RIGHT, expand=True, fill='both')
+img = ImageTk.PhotoImage(Image.open(r"C:\Users\Vladimir\Desktop\test.jpg"))
+lbl = Label(leftFrame, text="Choose item", font=("Arial Bold", 40), bg="blue")
+lbl.pack(expand=True,fill='both')
+txt = Entry(leftFrame, width=10, bg="yellow", font=("Arial Bold", 40), fg="black")
+txt.pack(expand=True,fill='both')
 btn1 = Button(leftFrame, text="Add to the basket", font=("Arial Bold", 25), bg="blue", fg="black", command=register)
 btn2 = Button(leftFrame, text="Void", font=("Arial Bold", 25), bg="blue", fg="black", command=void)
 btn3 = Button(leftFrame, text="Subtotal", font=("Arial Bold", 25), bg="blue", fg="black", command=subtotal)
@@ -169,7 +164,52 @@ btn10.pack(expand=True,fill='both')
 btn11.pack(expand=True,fill='both')
 
 
-root.mainloop()
+#Functions login
+
+def command1():
+    if entry1.get() == "user" and entry2.get() == "password": #Checks whether username and password are correct
+        root.deiconify() #Unhides the root window
+        top.destroy() #Removes the toplevel window
+
+def command2():
+    top.destroy() #Removes the toplevel window
+    root.destroy() #Removes the hidden root window
+    sys.exit() #Ends the script
+
+
+entry1.pack() #These pack the elements, this includes the items for the main window
+entry2.pack()
+button1.pack()
+button2.pack()
+
+
+root.withdraw() #This hides the main window, it's still present it just can't be seen or interacted with
+root.mainloop() #Starts the event loop for the main window
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
